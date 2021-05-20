71a29b657ef2605b08f202d40ef625b0923b3276
Loris Mancel <lm@taktik.com>
2021-05-20 15:57:12 +0200

    feat(parsing) stream to retrieve trackinfo

--- lib/mux.js.d.ts
+++ lib/mux.js.d.ts
@@ -1,12 +1,43 @@
 declare module '@taktik/mux.js' {
-  import { Stream } from 'stream'
-
-  interface ITransmuxerOptions {
-    baseMediaDecodeTime?: number
-    keepOriginalTimestamps?: boolean
-    remux?: boolean
+  import { Stream, Transform } from 'stream'
+  interface ITools {
+    inspect(data: Buffer): any
+    findBox(data: Buffer, boxes: string[]): Buffer[]
   }
 
+  interface ITrackMetadata {
+    id: number
+    type: string
+    codec?: string
+    timescale?: number
+  }
+
+  interface IOutputTrack<T extends 'audio' | 'video'> {
+    initSegment?: Buffer
+    data: Buffer
+    type: T
+    codec: string
+    pid: number
+  }
+
+  interface ITransmuxerOutput {
+    type: 'combined'
+    audio: IOutputTrack<'audio'>[]
+    video: IOutputTrack<'video'>
+  }
+
+  interface ITrack {
+    id?: number
+    type?: string
+    codec?: string
+    timescale?: number
+  }
+
+  type TrackInfo = {
+    audio: { codec: string, pid: number, languages: { code: string }[], type: 'audio' }[]
+    video: { type: 'video', pid: number, codec: string } | undefined
+    subtitles: { type: 'subtitles', pid: number, code: string }[]
+  }
   interface IAudioTrackLanguage {
     code: string
     type: number
@@ -17,6 +48,11 @@ declare module '@taktik/mux.js' {
     type: 'audio'
     codec: string
     languages: IAudioTrackLanguage[]
+  }
+  interface ITransmuxerOptions {
+    baseMediaDecodeTime?: number
+    keepOriginalTimestamps?: boolean
+    remux?: boolean
   }
 
   class Transmuxer extends Stream {
@@ -38,42 +74,8 @@ declare module '@taktik/mux.js' {
     setAudioTrackFromPid: (pid: number) => void
     canFlush(): boolean
   }
-
-  interface ITools {
-    inspect: (data: Buffer) => any
-  }
-
-  export interface ITrackMetadata {
-    id: number
-    type: string
-    codec?: string
-    timescale?: number
-  }
-
-  interface IOutputTrack<T extends 'audio' | 'video'> {
-    initSegment: Buffer
-    metadata: ITrackMetadata[]
-    data: Buffer
-    type: T
-  }
-
-  export interface ITransmuxerOutput {
-    type: 'combined'
-    audio: IOutputTrack<'audio'>[]
-    video: IOutputTrack<'video'>
-  }
-
-  export interface ITrack {
-    id?: number
-    type?: string
-    codec?: string
-    timescale?: number
-  }
-
-  export type TrackInfo = {
-    audio: { codec: string, pid: number, languages: { code: string, type: number }[], type: 'audio' }[]
-    video: { type: 'video', pid: number, codec: string } | undefined
-    subtitles: { type: 'subtitles', pid: number, code: string }[]
+  export class TrackInfoStream extends Transform {
+    constructor()
   }
 
   export const mp4: { Transmuxer: typeof Transmuxer, tools: ITools, probe: { tracks: (initSegment: Buffer) => ITrack[] } }
